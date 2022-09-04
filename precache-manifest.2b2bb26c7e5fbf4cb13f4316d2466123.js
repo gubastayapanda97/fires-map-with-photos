61ece4e1fb919460e56fba1862cb1ff2333d0185
Gubastaya Panda <v.paksha@gaskar.group>
2022-09-04 22:06:05 +0300

    Updates

---
+++ precache-manifest.2b2bb26c7e5fbf4cb13f4316d2466123.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2333c09e2e7433f76f99b7e6e47d19fd",
+    "url": "/fires-map-with-photos/index.html"
+  },
+  {
+    "revision": "ac01468d365ddd80038b",
+    "url": "/fires-map-with-photos/static/css/2.84665c66.chunk.css"
+  },
+  {
+    "revision": "f9c267049e2743010edc",
+    "url": "/fires-map-with-photos/static/css/main.7fc5b926.chunk.css"
+  },
+  {
+    "revision": "ac01468d365ddd80038b",
+    "url": "/fires-map-with-photos/static/js/2.9442109a.chunk.js"
+  },
+  {
+    "revision": "f9c267049e2743010edc",
+    "url": "/fires-map-with-photos/static/js/main.87af6652.chunk.js"
+  },
+  {
+    "revision": "0ba9b5390db60dc8479a",
+    "url": "/fires-map-with-photos/static/js/runtime-main.796f40cc.js"
+  },
+  {
+    "revision": "4529c537caf613bdd7e314648486a994",
+    "url": "/fires-map-with-photos/static/media/1.4529c537.png"
+  },
+  {
+    "revision": "c3ed7ff74201b3ca59fa0bdff2350c64",
+    "url": "/fires-map-with-photos/static/media/10.c3ed7ff7.png"
+  },
+  {
+    "revision": "197686ce1f5d6723f7997158a17735d0",
+    "url": "/fires-map-with-photos/static/media/11.197686ce.png"
+  },
+  {
+    "revision": "b72155bf9f04e5c389491b2c93662973",
+    "url": "/fires-map-with-photos/static/media/12.b72155bf.png"
+  },
+  {
+    "revision": "7324092dfc280059a4a423609f162cd8",
+    "url": "/fires-map-with-photos/static/media/13.7324092d.png"
+  },
+  {
+    "revision": "a2948468f15e2e07a18a516cf847452a",
+    "url": "/fires-map-with-photos/static/media/14.a2948468.png"
+  },
+  {
+    "revision": "a5e43a8416118713164f1266786a1f38",
+    "url": "/fires-map-with-photos/static/media/15.a5e43a84.png"
+  },
+  {
+    "revision": "1506d5f66ebc3c7a916734bc09a57818",
+    "url": "/fires-map-with-photos/static/media/16.1506d5f6.png"
+  },
+  {
+    "revision": "b199599cd3f850c9944e20142b8884ac",
+    "url": "/fires-map-with-photos/static/media/17.b199599c.png"
+  },
+  {
+    "revision": "13eb519fea163d53dd2631e7d84b3870",
+    "url": "/fires-map-with-photos/static/media/18.13eb519f.png"
+  },
+  {
+    "revision": "2c98bb8bd065335550727bc59a7d2b20",
+    "url": "/fires-map-with-photos/static/media/19.2c98bb8b.png"
+  },
+  {
+    "revision": "873976453510377b35c332fedb604198",
+    "url": "/fires-map-with-photos/static/media/2.87397645.png"
+  },
+  {
+    "revision": "cd00b7d343cc816103fe84254f776feb",
+    "url": "/fires-map-with-photos/static/media/20.cd00b7d3.png"
+  },
+  {
+    "revision": "a9d8a3e8d3138b6c18cfc202b2d90b32",
+    "url": "/fires-map-with-photos/static/media/21.a9d8a3e8.png"
+  },
+  {
+    "revision": "08cdac67bac10716ac18997f7bb6b262",
+    "url": "/fires-map-with-photos/static/media/22.08cdac67.png"
+  },
+  {
+    "revision": "be67d5d3df093b8b0a483c70999a9d52",
+    "url": "/fires-map-with-photos/static/media/23.be67d5d3.png"
+  },
+  {
+    "revision": "4098234ace83950ecd52dbc35a5a2676",
+    "url": "/fires-map-with-photos/static/media/24.4098234a.png"
+  },
+  {
+    "revision": "f12419a4e30f4922727527e424a4341b",
+    "url": "/fires-map-with-photos/static/media/25.f12419a4.png"
+  },
+  {
+    "revision": "f0225aac326ccd072a09a0c30ccbfc7c",
+    "url": "/fires-map-with-photos/static/media/3.f0225aac.png"
+  },
+  {
+    "revision": "85aebde28e1da0920652b514db5b3a0b",
+    "url": "/fires-map-with-photos/static/media/4.85aebde2.png"
+  },
+  {
+    "revision": "ee62442b7d3f9c6fb4097f3e5468a956",
+    "url": "/fires-map-with-photos/static/media/5.ee62442b.png"
+  },
+  {
+    "revision": "b9ddc08bd4a85ac7ebb60b76f82af735",
+    "url": "/fires-map-with-photos/static/media/6.b9ddc08b.png"
+  },
+  {
+    "revision": "44a1d9e5613ef5867bd1e6f1fb27cccc",
+    "url": "/fires-map-with-photos/static/media/7.44a1d9e5.png"
+  },
+  {
+    "revision": "0dd202af340fdaea8b06e46603c7f02b",
+    "url": "/fires-map-with-photos/static/media/8.0dd202af.png"
+  },
+  {
+    "revision": "d0dabd5eef1da1515a4958b72f524038",
+    "url": "/fires-map-with-photos/static/media/9.d0dabd5e.png"
+  },
+  {
+    "revision": "2ceaf661f7c099123415f94f97e2a0c5",
+    "url": "/fires-map-with-photos/static/media/venue_location_icon.2ceaf661.svg"
+  }
+]);
